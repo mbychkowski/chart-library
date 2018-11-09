66513d8d486d805c0d07e4f4ac295f94e26b4af0
michael bychkowski <mbychkowski@gmail.com>
2018-11-09 09:28:14 -0500

    updates to line chart

--- line-chart/script.js
+++ line-chart/script.js
@@ -1,4 +1,13 @@
+var dataset,
+    xScale,
+    yScale,
+    line;
+
+var svgWidth = 800, svgHeight = 300;
+var padding = 40;
+
 var rowConverter = function(d) {
+    
     return {
         // make new data object for each year + month
         date: new Date(+d.year, (+d.month - 1)),
@@ -7,11 +16,36 @@ var rowConverter = function(d) {
 }
 
 //load data 
-d3.csv('mauna_loa_co2_monthly_averages.csv', rowConverter, function(data) {
-
-    var dataset = data;
+d3.csv('mauna_loa_co2_monthly_averages.csv', rowConverter)
+    .then(function(dataset) {
 
     // print data to console as table for verification
-    console.table(dataset, ['date', 'average']);
-});
+    // console.table(dataset, ['date', 'average']);
 
+    xScale = d3.scaleTime()
+        .domain([
+            d3.min(dataset, function(d) { return d.date; }),
+            d3.max(dataset, function(d) { return d.date; })
+        ])
+        .range([0, svgWidth]);
+
+    yScale = d3.scaleLinear()
+        .domain([
+            d3.min(0, d3.max(dataset, function(d) { return d.average; }))
+        ])
+        .range([svgHeight, 0]);
+
+    line = d3.line()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.average); });
+
+    var svg = d3.select('body')
+        .append('svg')
+        .attr('width', svgWidth)
+        .attr('height', svgHeight);
+
+    svg.append('path')
+        .datum(dataset)
+        .attr('class', 'line')
+        .attr('d', line);
+});
